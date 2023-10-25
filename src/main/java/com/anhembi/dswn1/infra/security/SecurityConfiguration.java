8c8294068a0efdb990ac93e5a328742ca2f9f1b0
Camila Caetano <camilaanjos478@gmail.com>
2023-10-25 14:44:24 -0300

    feat.:bloqueando usuarios sem a role de ADMIN para operar na api deletando, atualizando e postando

--- src/main/java/com/anhembi/dswn1/infra/security/SecurityConfiguration.java
+++ src/main/java/com/anhembi/dswn1/infra/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.anhembi.dswn1.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +16,12 @@ public class SecurityConfiguration {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.PUT, "/pets").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/pets").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/pets").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
                 .build();
     }
 }
