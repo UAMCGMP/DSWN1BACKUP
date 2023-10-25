b3af8d02ae7e4cc6532c7bb9c761ecec4ffe6a45
Camila Caetano <camilaanjos478@gmail.com>
2023-10-25 13:13:45 -0300

    feat.: add nova migration para adicionar um tabela de usuario e criação da entidade

---
+++ src/main/java/com/anhembi/dswn1/domain/user/User.java
@@ -0,0 +1,56 @@
+package com.anhembi.dswn1.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Table(name = "users")
+@Entity(name = "users")
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    private String id;
+    private String login;
+    private String password;
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
