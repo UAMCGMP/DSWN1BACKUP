ae988de81942fdae1a57aebe8e4467403e254851
Camila Caetano <camilaanjos478@gmail.com>
2023-10-24 22:20:46 -0300

    chore.: creating project structure

---
+++ src/test/java/com/anhembi/dswn1/Dswn1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhembi.dswn1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dswn1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
