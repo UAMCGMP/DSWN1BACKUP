ff3e9c1af1aedb26aefb092ac0019965fa6ea407
Camila Caetano <camilaanjos478@gmail.com>
2023-11-15 14:39:40 -0300

    fix.: remove de import não utilizado

--- src/main/java/com/anhembi/dswn1/infra/security/SecurityFilter.java
+++ src/main/java/com/anhembi/dswn1/infra/security/SecurityFilter.java
@@ -1,7 +1,6 @@
 package com.anhembi.dswn1.infra.security;
 
 import com.anhembi.dswn1.repository.UserRepository;
-import com.auth0.jwt.JWT;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
